17e71ba7a9aa19bc7b8a845774679b5eae41739b
jal <jxmes.long@outlook.com>
2026-02-04 18:12:40 +0000

    small changes

--- flappy_guy.c
+++ flappy_guy.c
@@ -20,67 +20,66 @@ Color colors[MAX_COLORS_COUNT] = {
 
 // init flapper
 typedef struct {
-  float x, y;
-  float velocity;
-  int alive; // 0 - dead, 1 - alive, 2 - dying
+  float     x, y;
+  float     velocity;
+  int       alive; // 0 - dead, 1 - alive, 2 - dying
   Rectangle rect;
   Texture2D texture;
 } Flapper;
 
 // init pipes
 typedef struct {
-  float x, y;
+  float     x, y;
   Rectangle rect;
-  Texture texture;
-  bool active;
+  Texture   texture;
+  bool      active;
 } Pipe;
 
-void close_game(){
- // UnloadMusicStream(*soundtrack);
+void start_game(){
+  // @todo
+}
+
+void close_game(Music *soundtrack, Texture2D *cloud, Texture2D *pipeTexture){
+  UnloadMusicStream(*soundtrack);
+  UnloadTexture(*pipeTexture);
+  UnloadTexture(*cloud);
   CloseAudioDevice();
   CloseWindow();
 }
 
-void main_menu(bool *playing, bool *exit_game){
+void main_menu(bool *playing, bool *should_exit_game){
   static int currentItem = 0;
-  static int menuCount = 3;
+  static int menuCount   = 3;
 
   if(IsKeyPressed(KEY_DOWN)){
     currentItem++;
     if(currentItem>=menuCount) currentItem = 0;
-    printf("currentitem=%d\n", currentItem);
   }
 
   if(IsKeyPressed(KEY_UP)){
     currentItem--;
     if(currentItem<0) currentItem = menuCount-1;
-    printf("currentitem=%d\n", currentItem);
   }
 
   if(currentItem == 0 && IsKeyPressed(KEY_ENTER)) *playing = true;
-  
-  if(currentItem == 2 && IsKeyPressed(KEY_ENTER)) *exit_game = true;
+  if(currentItem == 2 && IsKeyPressed(KEY_ENTER)) *should_exit_game = true;
 
   ClearBackground(SKYBLUE);
-  DrawText("PLAY", SCREENWIDTH/2, SCREENHEIGHT/4, 50, currentItem == 0 ? WHITE : BLACK);
+  DrawText("PLAY",     SCREENWIDTH/2, SCREENHEIGHT/4, 50, currentItem == 0 ? WHITE : BLACK);
   DrawText("SETTINGS", SCREENWIDTH/2, SCREENHEIGHT/3, 50, currentItem == 1 ? WHITE : BLACK);
-  DrawText("QUIT", SCREENWIDTH/2, SCREENHEIGHT/2, 50, currentItem == 2 ? WHITE : BLACK);
+  DrawText("QUIT",     SCREENWIDTH/2, SCREENHEIGHT/2, 50, currentItem == 2 ? WHITE : BLACK);
 }
 
 
 Flapper Flapper_Init(float x, float y) {
-  Flapper f = {0};
-  f.x = x;
-  f.y = y;
+  Flapper f  = {0};
+  f.x        = x;
+  f.y        = y;
   f.velocity = 0.0f;
   f.alive    = 1;
-  f.texture = LoadTexture("./assets/flapper1.png");
-  f.rect = (Rectangle){
-    .x = f.texture.width  * 0.1f,
-    .y = f.texture.height * 0.25f,
-    .width  = f.texture.width * 0.8f,
-    .height = f.texture.height * 0.5f,
-  };
+  f.texture  = LoadTexture("./assets/flapper1.png");
+  f.rect = (Rectangle){ f.x, f.y, 40, 40};
+
   if(!f.alive){
     UnloadTexture(f.texture);
   }
@@ -89,12 +88,14 @@ Flapper Flapper_Init(float x, float y) {
 
 void Init_Pipes(Pipe pipes[], int count, float startX, Texture2D pipeTexture) {
   for(int i=0; i < count; i++){
-    pipes[i].rect.x = startX + i * PIPE_SPACING;
-    pipes[i].rect.y = GetRandomValue(SCREENHEIGHT-300, SCREENHEIGHT-100);
-    pipes[i].texture = pipeTexture;
-    pipes[i].rect.width = pipes[i].texture.width;
-    pipes[i].rect.height = pipes[i].texture.height;
-    pipes[i].active = true;
+    pipes[i].x           = (startX + i * PIPE_SPACING);
+    pipes[i].y           = (GetRandomValue(SCREENHEIGHT-300, SCREENHEIGHT-100));
+    pipes[i].rect.x      = pipes[i].x + 125;
+    pipes[i].rect.y      = pipes[i].y + 120;
+    pipes[i].texture     = pipeTexture;
+    pipes[i].rect.width  = pipes[i].texture.width/7;
+    pipes[i].rect.height = pipes[i].texture.height/4;
+    pipes[i].active      = true;
   }
 }
 
@@ -102,34 +103,37 @@ void flapper_movement(Flapper *flapper) {
   static float velY     = 0.0f;
   const float gravity   = 0.5f;
   const float jumpForce = -10.0f;
-
-  while(flapper->alive){
-    flapper->rect.x += 1;
+  
+  if(flapper->alive){
+    flapper->x += 1;
 
     if(IsKeyPressed(KEY_SPACE)){
       velY = jumpForce;
     }
 
     velY += gravity;
-    flapper->y += velY;
-    flapper->rect.y = flapper->y;
-    flapper->x = flapper->rect.x;
-    break;
+    flapper->y     += velY;
+    flapper->rect.y = flapper->y + 75.0f;
+    flapper->rect.x = flapper->x + 100.0f;
   }
 }
 
 void collisions(Flapper *flapper, Pipe *pipes) {
   //collisions with the sky and floor
   if(flapper->rect.y <= 0 || flapper->rect.y+flapper->rect.height >= SCREENHEIGHT) { // the subtractions are because the models rectangle "hitbox" is bigger than the texture
+    printf("------------\n");
+    printf("Hit the floor or ceiling\n");
+    printf("------------\n");
     flapper->alive = 0;
   }
-  printf("Flapper.topedge=%.1f -- flapper.bottomedge=%.1f\n", flapper->rect.y, flapper->rect.y+256);
 
   // collisions with pipes
   for(int i=0; i<PIPE_COUNT; i++){
     if(CheckCollisionRecs(flapper->rect, pipes[i].rect)){
       flapper->alive = 0;
-      printf("Alive=%d\n", flapper->alive);
+      printf("------------\n");
+      printf("Hit a pipe\n");
+      printf("------------\n");
     }
   }
 }
@@ -137,7 +141,7 @@ void collisions(Flapper *flapper, Pipe *pipes) {
 int main(void) {
 
   bool playing   = false;
-  bool exit_game = false;
+  bool should_exit_game = false;
 
   InitWindow(SCREENWIDTH, SCREENHEIGHT, "Flappy Guy");
   SetTargetFPS(60);
@@ -168,7 +172,7 @@ int main(void) {
   camera.rotation = 0;
   camera.zoom     = 1.0f;
 
-  while (!WindowShouldClose() && !exit_game) { // game loop
+  while (!WindowShouldClose() && !should_exit_game) { // game loop
 
     camera.target.x = flapper.x+500;
 
@@ -183,27 +187,27 @@ int main(void) {
     ClearBackground(SKYBLUE);
 
     if(!playing){
-      main_menu(&playing, &exit_game);
+      main_menu(&playing, &should_exit_game);
       EndDrawing();
     }else{
       flapper_movement(&flapper);
-      collisions(&flapper, &pipes);
+      collisions(&flapper, pipes);
+      HideCursor();
       
       BeginMode2D(camera);
       
       DrawTexture(cloud, 40, 20, WHITE);
       DrawRectangleLines(flapper.rect.x, flapper.rect.y, flapper.rect.width, flapper.rect.height, BLACK);
-      
       // draw pipes
       for(int i=0; i<PIPE_COUNT; i++){
         if(!pipes[i].active) continue;
-        DrawTexture(pipes[i].texture, pipes[i].rect.x, pipes[i].rect.y, WHITE);
-        DrawRectangleLines(pipes[i].rect.x, pipes[i].rect.y, pipes[i].rect.width/2, pipes[i].rect.height/2, BLACK);
+        DrawTexture(pipes[i].texture, pipes[i].x, pipes[i].y, WHITE);
+        DrawRectangleLines(pipes[i].rect.x, pipes[i].rect.y, pipes[i].rect.width, pipes[i].rect.height, BLACK);
       }
 
       if(!flapper.alive){
         playing = false;
-
+        ShowCursor();
       }
 
       // printf("flapper.x=%f flapper.y=%f\n flapperrect.x%f flapperrect.height%f\n flapper.alive=%i\n", flapper.x, flapper.y, flapper.rect.x, flapper.rect.height, flapper.alive); // debug line
@@ -213,6 +217,6 @@ int main(void) {
       EndDrawing();
     }
   }
-  close_game();
+  close_game(&soundtrack, &cloud, &pipeTexture);
 }
 
---
+++ todo.md
@@ -0,0 +1,28 @@
+    /---------/
+   /  TODO   /
+  /---------/
+ / (  ^.^ )/
+/---------/
+
+
+1. Add mouse control to menu
+
+2. Restart game after death
+
+2. ~~Hide cursor when game is being played~~
+
+3. Cycle through flappy textures when jumping
+
+4. ~~Fix the hitboxes for pipes and flapper~~
+
+5. Implement scoring
+ 
+
+
+
+
+
+
+
+
+
